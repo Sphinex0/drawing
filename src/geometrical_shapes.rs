35a7f57a97f34b498209c999b15e1e5a12e052b7
aelmir <adnane.elmir1@gmail.com>
2025-05-13 18:26:31 +0100

    triangle, rectangle, circle finished

--- src/geometrical_shapes.rs
+++ src/geometrical_shapes.rs
@@ -17,11 +17,14 @@ pub trait Drawable {
 }
 
 #[derive(Debug, Clone)]
-pub struct Point(i32, i32);
+pub struct Point {
+    x: i32,
+    y: i32,
+}
 
 impl Point {
     pub fn new(x: i32, y: i32) -> Self {
-        Point(x, y)
+        Point { x, y }
     }
 
     pub fn random(width: i32, height: i32) -> Self {
@@ -32,17 +35,25 @@ impl Point {
 
 impl Drawable for Point {
     fn draw(&self, image: &mut Image) {
-        image.display(self.0, self.1, self.color());
+        image.display(self.x, self.y, self.color());
     }
 }
 
-
-
-pub struct Line(Point, Point);
+pub struct Line {
+    p1: Point,
+    p2: Point,
+    color: Color,
+}
 
 impl Line {
     pub fn new(p1: &Point, p2: &Point) -> Self {
-        Line(p1.clone(), p2.clone())
+        let mut line = Self {
+            p1: p1.clone(),
+            p2: p2.clone(),
+            color: Color::white(),
+        };
+        line.color = line.color();
+        line
     }
 
     pub fn random(width: i32, height: i32) -> Self {
@@ -52,31 +63,141 @@ impl Line {
 
 impl Drawable for Line {
     fn draw(&self, image: &mut Image) {
-        let x1 = self.0.0 ;
-        let x2 = self.1.0 ;
-        let y1 = self.0.1 ;
-        let y2 = self.1.1 ;
-        let x_diff = x2-x1;
-        let y_diff = y2-y1;
+        let x1 = self.p1.x;
+        let x2 = self.p2.x;
+        let y1 = self.p1.y;
+        let y2 = self.p2.y;
+        let x_diff = (x2 - x1).abs();
+        let y_diff = (y2 - y1).abs();
 
-        let max:i32 = x_diff.max(y_diff);
-        let mut current_x = x1.into();
-        let mut current_y= y1.into();
-        
-        for _ in 0..max{
-            image.display(current_x, current_y, self.color());
-            current_x += x_diff/max;
-            current_y += y_diff/max;
+        let max: i32 = x_diff.max(y_diff);
+        let mut current_x: f64 = x1.into();
+        let mut current_y: f64 = y1.into();
+        for _ in 0..max {
+            image.display(current_x as i32, current_y as i32, self.color.clone());
+            if x1 < x2 {
+                current_x += x_diff as f64 / max as f64;
+            } else {
+                current_x -= x_diff as f64 / max as f64;
+            }
+
+            if y1 < y2 {
+                current_y += y_diff as f64 / max as f64;
+            } else {
+                current_y -= y_diff as f64 / max as f64;
+            }
         }
-
-        //image.display(self.0, self.1, self.color());
     }
 }
 
-// pub struct Triangle(Point, Point, Point);
+pub struct Triangle {
+    a: Point,
+    b: Point,
+    c: Point,
+}
 
-// impl Triangle {
-//     pub fn new(p1: Point, p2: Point, p3: Point) -> Self {
-//         Triangle(p1, p2, p3)
-//     }
-// }
+impl Triangle {
+    pub fn new(p1: &Point, p2: &Point, p3: &Point) -> Self {
+        Triangle {
+            a: p1.clone(),
+            b: p2.clone(),
+            c: p3.clone(),
+        }
+    }
+}
+
+impl Drawable for Triangle {
+    fn draw(&self, image: &mut Image) {
+        let color = self.color();
+        let mut line1 = Line::new(&self.a, &self.b);
+        let mut line2 = Line::new(&self.b, &self.c);
+        let mut line3 = Line::new(&self.c, &self.a);
+        line1.color = color.clone();
+        line2.color = color.clone();
+        line3.color = color.clone();
+
+        line1.draw(image);
+        line2.draw(image);
+        line3.draw(image);
+    }
+}
+
+pub struct Rectangle {
+    left_p: Point,
+    right_p: Point,
+}
+
+impl Rectangle {
+    pub fn new(p1: &Point, p2: &Point) -> Self {
+        Rectangle {
+            left_p: p1.clone(),
+            right_p: p2.clone(),
+        }
+    }
+}
+
+impl Drawable for Rectangle {
+    fn draw(&self, image: &mut Image) {
+        let color = self.color();
+
+        let point1 = &Point::new(self.left_p.x, self.right_p.y);
+        let point2 = &Point::new(self.right_p.x, self.left_p.y);
+        let mut line1 = Line::new(&self.left_p, point1);
+        let mut line2 = Line::new(point1, &self.right_p);
+        let mut line3 = Line::new(&self.right_p, point2);
+        let mut line4 = Line::new(point2, &self.left_p);
+
+        line1.color = color.clone();
+        line2.color = color.clone();
+        line3.color = color.clone();
+        line4.color = color.clone();
+
+        line1.draw(image);
+        line2.draw(image);
+        line3.draw(image);
+        line4.draw(image);
+    }
+}
+
+pub struct Circle {
+    center: Point,
+    radius: i32,
+}
+impl Circle {
+    pub fn new(p1: &Point, r: i32) -> Self {
+        Circle {
+            center: p1.clone(),
+            radius: r,
+        }
+    }
+
+    pub fn random(width: i32, height: i32) -> Self {
+        Self::new(
+            &Point::random(width, height),
+            rand::rng().random_range(0..width.min(height)),
+        )
+    }
+}
+
+impl Drawable for Circle {
+    fn draw(&self, image: &mut Image) {
+        let angle = 0f64;
+        let mut last_x = ((self.radius as f64 * angle.cos() as f64) + self.center.x as f64) as i32;
+        let mut last_y = ((self.radius as f64 * angle.sin() as f64) + self.center.y as f64) as i32;
+        let color = self.color();
+
+        for i in 0..=360 {
+            let last_point = Point::new(last_x, last_y);
+            let angle = (i as f64) * std::f64::consts::PI / 180.0;
+            let x = ((self.radius as f64 * angle.cos() as f64) + self.center.x as f64) as i32;
+            let y = ((self.radius as f64 * angle.sin() as f64) + self.center.y as f64) as i32;
+            last_x = x;
+            last_y = y;
+
+            // image.display(x, y, color.clone());
+            let mut line = Line::new(&last_point, &Point::new(x, y));
+            line.color = color.clone();
+            line.draw(image);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,35 @@ use raster::{Color, Image};
 fn main() {
     let mut image = Image::blank(1000, 1000);
 
-    // gs::Line::random(image.width, image.height).draw(&mut image);
+    gs::Line::random(image.width, image.height).draw(&mut image);
+
+    //line test
     for _ in 0..2000{
 
         gs::Point::random(image.width, image.height).draw(&mut image);
     }
 
-    // let rectangle = gs::Rectangle::new(&gs::Point::new(150, 150), &gs::Point::new(50, 50));
-    // rectangle.draw(&mut image);
 
-    // let triangle = gs::Triangle::new (
-    //         &gs::Point::new(500, 500),
-    //         &gs::Point::new(250, 700),
-    //         &gs::Point::new(700, 800),
-    // );
-    // triangle.draw(&mut image);
+    let ponit1 = gs::Point::new(500,500);
+    let ponit2 = gs::Point::new(0, 700);
 
-    // for _ in 1..50 {
-    //     gs::Circle::random(image.width, image.height).draw(&mut image);
-    // }
+    let line1 = gs::Line::new(&ponit1 , &ponit2);
+    line1.draw(&mut image);
 
+    let rectangle = gs::Rectangle::new(&gs::Point::new(150, 300), &gs::Point::new(50, 50));
+    rectangle.draw(&mut image);
+
+    let triangle = gs::Triangle::new (
+            &gs::Point::new(500, 500),
+            &gs::Point::new(250, 700),
+            &gs::Point::new(700, 800),
+    );
+    triangle.draw(&mut image);
+
+    for _ in 1..50 {
+        gs::Circle::random(image.width, image.height).draw(&mut image);
+    }
+    gs::Circle::new(&gs::Point::new(500, 500), 500).draw(&mut image);
     raster::save(&image, "image.png").unwrap();
 }
 
